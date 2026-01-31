dd62a3933657150188bf50ce8168e949b390b879
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-01-31 19:29:27 +0200

    chore: update domain DTOs and schema

--- packages/domain/src/user/dto/update-user.dto.ts
+++ packages/domain/src/user/dto/update-user.dto.ts
@@ -1,10 +1,8 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { IsObject, IsOptional } from 'class-validator';
 import { RegisterUserDto } from './register-user.dto';
 
-export class UpdateUserDto extends PartialType(
-  OmitType(RegisterUserDto, ['email', 'isSystem'] as const),
-) {
+export class UpdateUserDto extends PartialType(RegisterUserDto) {
   @IsOptional()
   @IsObject()
   settingsConfig?: Record<string, unknown>;
