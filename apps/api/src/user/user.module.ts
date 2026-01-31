4b8f7b705f92801e6e1e7ea880950f4b92485a61
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-01-31 19:26:57 +0200

    refactor(api): remove system user and use JWT auth

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { SystemUserProvider } from './system-user.provider';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, SystemUserProvider],
+  providers: [UserService],
   exports: [UserService],
 })
 export class UserModule {}
