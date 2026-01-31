8bd68b48668052bfbef421b2b0fdd0b1fdd8335e
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-01-31 16:35:48 +0200

    feat(api): add sections module with basic CRUD

--- packages/domain/src/entry.ts
+++ packages/domain/src/entry.ts
@@ -8,11 +8,18 @@ import { CreateSpaceDto } from './space/dto/create-space.dto';
 import { UpdateSpaceDto } from './space/dto/update-space.dto';
 import { Space } from './space/entities/space.entity';
 
+import { CreateSectionDto } from './section/dto/create-section.dto';
+import { UpdateSectionDto } from './section/dto/update-section.dto';
+import { Section } from './section/entities/section.entity';
+
 export {
+  CreateSectionDto,
   CreateSpaceDto,
   LoginUserDto,
   RegisterUserDto,
+  Section,
   Space,
+  UpdateSectionDto,
   UpdateSpaceDto,
   UpdateUserDto,
   User,
