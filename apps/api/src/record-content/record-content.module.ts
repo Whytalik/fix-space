f43dc0c06e167c222a902ae701d3aa2b942c5463
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-02-01 21:32:23 +0200

    feat(api): implement property-value module with CRUD

---
+++ apps/api/src/record-content/record-content.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { RecordContentService } from './record-content.service';
+import { RecordContentController } from './record-content.controller';
+
+@Module({
+  controllers: [RecordContentController],
+  providers: [RecordContentService],
+  exports: [RecordContentService],
+})
+export class RecordContentModule {}
