7b45edc453f2925006f1681e31d0277e318e056a
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-02-14 21:55:12 +0200

    refactor(api): register infrastructure in app bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,10 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import 'reflect-metadata';
 
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
@@ -14,7 +15,15 @@ async function bootstrap() {
   const corsOrigin = config.get<string>('CORS_ORIGIN', 'http://localhost:3001');
 
   app.enableCors({ origin: corsOrigin });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(port);
   Logger.log(`API running at http://localhost:${port}`, 'Bootstrap');
 }
