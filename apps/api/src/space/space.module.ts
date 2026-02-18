222ec55c6c14360dd7e5cd96acd0749f6b8f6084
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-02-18 19:00:16 +0200

    refactor(api/space): extract SectionService and add DuplicateSpaceUseCase

--- apps/api/src/space/space.module.ts
+++ apps/api/src/space/space.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { InitializationConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
-import { InitializeUserSpaceUseCase } from './initialize-user-space.usecase';
+import { SettingsModule } from '../settings/settings.module';
+import { DuplicateSpaceUseCase } from './providers/duplicate-space.usecase';
+import { InitializeUserSpaceUseCase } from './providers/initialize-user-space.usecase';
+import { SectionService } from './providers/section.service';
 import { SpaceController } from './space.controller';
 import { SpaceService } from './space.service';
 
 @Module({
-  imports: [DatabaseModule, InitializationConfigModule],
+  imports: [DatabaseModule, InitializationConfigModule, SettingsModule],
   controllers: [SpaceController],
-  providers: [SpaceService, InitializeUserSpaceUseCase],
-  exports: [SpaceService, InitializeUserSpaceUseCase],
+  providers: [SpaceService, SectionService, InitializeUserSpaceUseCase, DuplicateSpaceUseCase],
+  exports: [SpaceService, SectionService, InitializeUserSpaceUseCase, DuplicateSpaceUseCase],
 })
-export class SpaceModule {}
+export class SpaceModule { }
