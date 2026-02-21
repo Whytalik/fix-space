86539078801bcea6f63ef6b12210fb24017eb555
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-02-21 12:01:45 +0200

    feat(property): register handlers in module and integrate registry into service

--- apps/api/src/property/property.module.ts
+++ apps/api/src/property/property.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { PropertyService } from './property.service';
 import { PropertyController } from './property.controller';
+import { PropertyService } from './property.service';
+import {
+  PROPERTY_TYPE_HANDLERS,
+  NumberHandler,
+  PropertyTypeRegistry,
+  TextHandler,
+} from './types';
+
+const handlers = [TextHandler, NumberHandler];
 
 @Module({
   controllers: [PropertyController],
-  providers: [PropertyService],
-  exports: [PropertyService],
+  providers: [
+    PropertyService,
+    PropertyTypeRegistry,
+    ...handlers,
+    {
+      provide: PROPERTY_TYPE_HANDLERS,
+      useFactory: (...handlerInstances: InstanceType<(typeof handlers)[number]>[]) =>
+        handlerInstances,
+      inject: handlers,
+    },
+  ],
+  exports: [PropertyService, PropertyTypeRegistry],
 })
 export class PropertyModule {}
