fb2fb4046ce55fdb1753226748ce1b6b3b07d233
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-02-21 22:00:53 +0200

    refactor(property): use domain type constants in handlers and remove local configs

--- apps/api/src/property/property.module.ts
+++ apps/api/src/property/property.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { PropertyController } from './property.controller';
 import { PropertyService } from './property.service';
 import {
-  PROPERTY_TYPE_HANDLERS,
   NumberHandler,
+  PROPERTY_TYPE_HANDLERS,
   PropertyTypeRegistry,
   TextHandler,
 } from './types';
@@ -25,4 +25,4 @@ const handlers = [TextHandler, NumberHandler];
   ],
   exports: [PropertyService, PropertyTypeRegistry],
 })
-export class PropertyModule {}
+export class PropertyModule { }
