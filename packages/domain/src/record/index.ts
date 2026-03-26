6aba1c480f5a2a8bccdf8032c9d09187b8725637
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-03-26 14:16:13 +0200

    feat(domain): export record filter, sort and search DTOs

--- packages/domain/src/record/index.ts
+++ packages/domain/src/record/index.ts
@@ -1,4 +1,7 @@
 export * from "./dto/create-record.dto";
+export * from "./dto/record-filter.dto";
 export * from "./dto/record-response.dto";
+export * from "./dto/record-sort.dto";
+export * from "./dto/space-search-response.dto";
 export * from "./dto/update-record.dto";
 export * from "./entities/record.entity";
