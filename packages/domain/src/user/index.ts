3d4c1d1a230ca3ebb7c50a21614d73dc76b405f6
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2026-05-30 20:33:24 +0300

    feat(domain): implement auth and user domain logic with configurations

--- packages/domain/src/user/index.ts
+++ packages/domain/src/user/index.ts
@@ -1,6 +1,4 @@
 export * from "./dto/change-password.dto";
-export * from "./dto/login-user.dto";
-export * from "./dto/register-user.dto";
 export * from "./dto/set-password.dto";
 export * from "./dto/update-user.dto";
 export * from "./dto/user-response.dto";
